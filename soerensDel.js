c6c21a95512b08a3c49331a89d228707e7575c08
Flax95 <95soren@gmail.com>
2018-09-24 10:27:44 +0200

    div i stedet for span

--- soerensDel.js
+++ soerensDel.js
@@ -1,8 +1,9 @@
-alert("Hi!");
+alert("Hej Per, jeg har nu hacket dig!");
 
 $(".test").css({
-	width: 50,
-	height: 50,
-	color: "pink",
+	width: "100px",
+	height: "100px",
+	background: "red",
+	margin: "100px auto",
 });
 
